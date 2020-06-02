eebc03852fd422c9af98eb40a510cd86085333cc
Paul Marshall <paul@stardog.com>
2020-06-02 10:16:11 -0500

    Change default password in helm post install hook (fixes #5)

--- charts/stardog/files/utils.sh
+++ charts/stardog/files/utils.sh
@@ -1,19 +1,20 @@
 
 function wait_for_start {
     (
-    PORT=${1}
+    HOST=${1}
+    PORT=${2}
     # Wait for stardog to be running
     RC=1
     COUNT=0
     set +e
     while [[ ${RC} -ne 0 ]];
     do
-      if [[ ${COUNT} -gt 90 ]]; then
+      if [[ ${COUNT} -gt 300 ]]; then
           return 1;
       fi
       COUNT=$(expr 1 + ${COUNT} )
       sleep 1
-      curl -v  http://localhost:${PORT}
+      curl -v  http://${HOST}:${PORT}/admin/healthcheck
       RC=$?
     done
     # Give it a second to finish starting up
@@ -26,26 +27,11 @@ function wait_for_start {
 function change_pw {
     (
     set +e
-
-    PORT=${1}
+    HOST=${1}
+    PORT=${2}
     NEW_PW=$(cat /etc/stardog-password/adminpw)
-
-    /opt/stardog/bin/stardog-admin --server http://localhost:${PORT} user list -u admin -p ${NEW_PW}
-    if [ $? -eq 0 ]; then
-        echo "Password already changed"
-        return 0
-    fi
-    /opt/stardog/bin/stardog-admin --server http://localhost:${PORT} user passwd -N ${NEW_PW}
+    /opt/stardog/bin/stardog-admin --server http://${HOST}:${PORT} user passwd -N ${NEW_PW}
     RC=$?
     return ${RC}
     )
-}
-
-function shutdown_stardog {
-(
-    PORT=${1}
-    THIS_PW=$(cat /etc/stardog-password/adminpw)
-    set -e
-    /opt/stardog/bin/stardog-admin --server http://localhost:${PORT} server stop -p ${THIS_PW}
-)
 }
